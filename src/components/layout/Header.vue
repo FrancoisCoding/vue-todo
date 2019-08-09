3ae60f26275acfe179e6f3295546a4703282ad13
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-08-09 01:57:11 -0400

    Added styling to Header component

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -11,6 +11,17 @@ export default {
 </script>
 
 <style scoped>
+.header {
+  background: #333;
+  color: #fff;
+  text-align: center;
+  padding: 10px;
+}
+
+.header a {
+  color: #fff;
+  padding-right: 5px;
+}
 </style>
 
 
