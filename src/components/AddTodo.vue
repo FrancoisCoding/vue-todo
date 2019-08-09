2681423ab429739ab2f5a6c74216d29651dbb714
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-08-09 02:00:52 -0400

    Created AddTodo component

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <form>
+      <input type="text" name="title" placeholder="Add Todo..." />
+      <input type="submit" class="btn" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddTodo"
+};
+</script>
